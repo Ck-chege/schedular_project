0d383229cdb49abe2ef1f45d5387944abd4c6480
ck-chege <kamauchege45@gmail.com>
2024-09-19 12:48:03 -0400

    Top navigation bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Bell, LogOut } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import SignOutButton from "./SignOutButton";
+import Link from "next/link";
 
 export default async function TopNavBar({ business_name }: { business_name: string }) {
 
@@ -10,9 +11,11 @@ export default async function TopNavBar({ business_name }: { business_name: stri
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-12">
           <div className="flex-shrink-0 flex items-center">
+            <Link href="/home" className="flex items-center">
             <span className="text-xl font-bold text-gray-800">
               {business_name}
             </span>
+            </Link>
           </div>
           <div className="flex items-center">
             <Button variant="ghost" size="icon" className="mr-4">
