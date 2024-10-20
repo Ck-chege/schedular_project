931753d59833fb3ea1aaea2d604cab083fbf2877
chege_izoh <kamauchege45@gmail.com>
2024-10-19 22:54:09 -0400

    Updates to use server create supabase func

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,37 +1,19 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+
+import { NextRequest, NextResponse } from 'next/server'
+import { createClient } from './server'
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   })
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll()
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({request,})
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          )
-        },
-      },
-    }
-  )
+  const supabase = createClient()
 
   // IMPORTANT: Avoid writing any logic between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
+  const {data: { user }} = await supabase.auth.getUser()
 
   if (
     !user &&
@@ -43,6 +25,26 @@ export async function updateSession(request: NextRequest) {
     url.pathname = '/login'
     return NextResponse.redirect(url)
   }
+
+  // const role = user?.user_metadata?.role
+
+  // if(role==='admin'){
+  //   const url = request.nextUrl.clone()
+  //   url.pathname = '/admin'
+  //   return NextResponse.redirect(url)
+  // }
+  // if(role==='manager'){
+  //   const url = request.nextUrl.clone()
+  //   url.pathname = '/home'
+  //   return NextResponse.redirect(url)
+  // }
+
+  // if(role==='employee'){
+  //   const url = request.nextUrl.clone()
+  //   url.pathname = '/employee'
+  //   return NextResponse.redirect(url)
+  // }
+
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
