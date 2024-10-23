58b559dc1acdd03526e4f4943f956b1c4fcfbab2
chege_izoh <kamauchege45@gmail.com>
2024-10-23 12:34:41 -0400

    refactor: refactor mapShiftTemplate and mapWorkdayConfigTemplate functions

--- components/TruncatedText.tsx
+++ components/TruncatedText.tsx
@@ -1,5 +1,3 @@
-import exp from "constants";
-
 const TruncatedText = ({ text }: { text: string }) => {
     return (
       <div className="max-h-[3em] overflow-hidden relative">
--- types/types.ts
+++ types/types.ts
@@ -20,3 +20,29 @@ export interface WorkdayConfigTemplateWithShiftsTemplate extends WorkdayConfigTe
   id: string
   shifts: ShiftTemplate[];
 }
+
+
+
+
+
+function mapShiftTemplate(shift: any): ShiftTemplate {
+  return {
+    id: shift.id,
+    duration: shift.duration,
+    startTime: shift.start_time,
+    endTime: shift.end_time,
+    title: shift.title,
+  };
+}
+
+function mapWorkdayConfigTemplate(shiftTemplateRow: any): WorkdayConfigTemplateWithShiftsTemplate {
+  return {
+    id: shiftTemplateRow.id,
+    title: shiftTemplateRow.title,
+    startTime: shiftTemplateRow.start_time,
+    endTime: shiftTemplateRow.end_time,
+    duration: shiftTemplateRow.duration,
+    shiftType: shiftTemplateRow.shift_type === 'fixed' ? 'fixed' : 'custom',
+    shifts: shiftTemplateRow.shifts.map((shift: any) => mapShiftTemplate(shift)), // assuming `shifts` is an array of JSON objects
+  };
+}
